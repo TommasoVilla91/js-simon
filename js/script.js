040442943114d0bcc33f7c08e84591b1b385a450
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-11 16:04:23 +0100

    feat: set timeout random number

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 // elementi
 const formElem = document.getElementById("user-form");
 const randomNumElem = document.getElementById("random-num");
+const resultElem = document.getElementById("result");
 
 // variabile input utente
 const allInputs = document.querySelectorAll("input[type=`number`]");
 
+// inserimento numero random
+randomNumElem.innerHTML = randomNum(10000, 99999);
+
+// timer 30 secondi numero random
+setTimeout(function() {
+    randomNumElem.innerHTML = "";
+}, 30000);
+
 // evento submit
 formElem.addEventListener("submit", function(event) {
-    setTimeout(function() {
-        
-    }, 30000);
-});
-
-// inserimento valori
-randomNumElem.innerHTML = randomNum(10000, 99999);
+    event.preventDefault();
+})
 
 // funzione numero random
 function randomNum(min, max) {
